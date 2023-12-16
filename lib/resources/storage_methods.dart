3bef776c1b607b25129ecd9cb391c4d826cd5c03
Lojain Mostafa <lojain17mostafa@gmail.com>
2023-12-17 01:36:28 +0200

    storage methods

---
+++ lib/resources/storage_methods.dart
@@ -0,0 +1,19 @@
+import 'dart:typed_data';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageMethods {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //adding image to firebase storage
+  Future<String> uploadImageToFirebaseStorage(
+      String childName, Uint8List file, bool isPost) {
+    Reference ref =
+        _storage.ref().child(childName).child(_auth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putData(file);
+  }
+}
