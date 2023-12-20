d0f28f7da374cef59addc2052f878d2011617283
Lojain Mostafa <lojain17mostafa@gmail.com>
2023-12-20 10:49:17 +0200

    chat

--- lib/screens/chat_messages.dart
+++ lib/screens/chat_messages.dart
@@ -27,78 +27,83 @@ class _ChatMessagesState extends State<ChatMessages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Chat Messages'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              // Handle adding a new chat
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => NewMessage()),
+        appBar: AppBar(
+          title: Text('Chat Messages'),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                // Handle adding a new chat
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NewMessage()),
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Handle searching (you can implement searching logic here)
+                print('Search button clicked!');
+              },
+            ),
+          ],
+        ),
+        body: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('chat')
+              .orderBy(
+                'createdAt',
+                descending: false,
+              )
+              .snapshots(),
+          builder: (ctx, chatSnapshots) {
+            if (chatSnapshots.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              // Handle searching (you can implement searching logic here)
-              print('Search button clicked!');
-            },
-          ),
-        ],
-      ),
-      body: StreamBuilder(
-        // Listen to the stream of messages
-        stream: FirebaseFirestore.instance
-            .collection('chat')
-            .orderBy(
-              'createdAt',
-              descending: false,
-            )
-            .snapshots(),
-        builder: (ctx, chatSnapshots) {
-          if (chatSnapshots.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
+            }
+
+            if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+              return const Center(
+                child: Text('No messages found.'),
+              );
+            }
+
+            final loadedMessages = chatSnapshots.data!.docs;
+            final firstMessageData = loadedMessages.first.data();
+            final firstUsername = firstMessageData['username'];
+
+            return ListView.builder(
+              itemCount: loadedMessages.length,
+              itemBuilder: (ctx, index) {
+                final messageData = loadedMessages[index].data();
+                final username = messageData['username'];
+                final text = messageData['text'];
+
+                // Only show the first username
+                if (index == 0) {
+                  return _buildChatMessage(firstUsername, text);
+                } else {
+                  // You can choose to skip or handle other messages as needed
+                  return Container();
+                }
+              },
             );
-          }
-
-          if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
-            return const Center(
-              child: Text('No messages found.'),
-            );
-          }
-
-          final loadedMessages = chatSnapshots.data!.docs;
-
-          return ListView.builder(
-            itemCount: loadedMessages.length,
-            itemBuilder: (ctx, index) {
-              final messageData = loadedMessages[index].data();
-              final username = messageData['username'];
-              final text = messageData['text'];
-              final userImage = messageData['userImage'];
-
-              return _buildChatMessage(username, text, userImage);
-            },
-          );
-        },
-      ),
-    );
+          },
+        ));
   }
 
-  Widget _buildChatMessage(String username, String text, String userImage) {
+  Widget _buildChatMessage(String username, String text) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CircleAvatar(
-            radius: 20,
-            backgroundImage: NetworkImage(userImage),
-          ),
+          // CircleAvatar(
+          //   radius: 20,
+          //   backgroundImage: NetworkImage(userImage),
+          // ),
           SizedBox(width: 8),
           Expanded(
             child: Column(
